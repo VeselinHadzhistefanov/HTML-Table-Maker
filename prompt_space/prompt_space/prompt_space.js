0d179799a78d4b0441e3f672955c4b1486dc063b
veselinhadzhistefanov <v.hadzhistefanov@gmail.com>
2026-02-13 15:23:54 +0200

    Provided more functionality and baseline classes for future improvements

--- prompt_space/prompt_space/prompt_space.js
+++ prompt_space/prompt_space/prompt_space.js
@@ -21,8 +21,11 @@
         var variant = sender === "machine" ? 2 : 1;
 
         var textWidth = measureTextWidth(text);
-        var bubbleWidth = Math.min(Math.ceil(textWidth), maxBubbleWidth);
         var isOverflowing = textWidth > maxBubbleWidth;
+
+        var bubbleWidth = Math.min(Math.ceil(textWidth), maxBubbleWidth);
+        var bubbleHeight = isOverflowing ? BOX_HEIGHT * 2 : BOX_HEIGHT
+
         var totalWidth = bubbleWidth + 2 * BOX_HEIGHT;
 
         var row = document.createElement("div");
@@ -31,16 +34,16 @@
         var container = document.createElement("div");
         container.className = "bubble-container";
         container.style.width = totalWidth + "px";
-        container.style.height = BOX_HEIGHT + "px";
+        container.style.height = bubbleHeight + "px";
 
-        container.appendChild(createBubbleSVG(bubbleWidth, BOX_HEIGHT, variant));
+        container.appendChild(createBubbleSVG(bubbleWidth, bubbleHeight, variant));
 
         var textDiv = document.createElement("div");
         textDiv.className = "bubble-text";
         textDiv.style.left = BOX_HEIGHT + "px";
         textDiv.style.top = "0";
         textDiv.style.width = bubbleWidth + "px";
-        textDiv.style.height = BOX_HEIGHT + "px";
+        textDiv.style.height = bubbleHeight + "px";
 
         var textSpan = document.createElement("span");
         textSpan.className = "bubble-text-content";
@@ -61,57 +64,37 @@
         messageArea.scrollTop = messageArea.scrollHeight;
     }
 
-    Object.prototype.translate = function (x, y) {
-        var point = [...this]
-        point[0] += x;
-        point[1] += y;
-
-        return point;
-    }
-
-    function createBubbleSVG(width, height, variant) {
-        var BOX = BOX_HEIGHT
-        var HEIGHT = Math.ceil(height / BOX_HEIGHT) * BOX_HEIGHT * 2;
-        var WIDTH = width;
+    function createBubbleSVG(width, height, direction) {
         var ns = "http://www.w3.org/2000/svg";
-
         var svg = document.createElementNS(ns, "svg");
-        svg.setAttribute("width", WIDTH + 2 * HEIGHT);
-        svg.setAttribute("height", HEIGHT);
+        svg.setAttribute("width", width + 2 * height);
+        svg.setAttribute("height", height);
         svg.setAttribute("overflow", "visible");
         svg.style.display = "block";
 
-        var topLeft = [0, 0]
-        var topRight = [BOX, 0]
-        var bottomLeft = [0, BOX]
-        var bottomRight = [BOX, BOX]
+        let shape = new Shape()
 
-        var lines = [
-            [...topRight, ...topRight.translate(WIDTH, 0), true],
-            [...bottomRight.translate(0, BOX), ...bottomRight.translate(WIDTH, BOX), true],
-            [...topLeft, ...topLeft.translate(0, HEIGHT - BOX), variant == 1],
-            [...bottomLeft, ...bottomLeft.translate(0, HEIGHT - BOX), variant == 2],
-            [...topLeft, ...topRight, variant === 1],
-            [...bottomLeft, ...bottomRight, variant === 2],
-            [...topLeft.translate(WIDTH, 0), ...topRight.translate(WIDTH, 0), variant === 2],
-            [...bottomLeft.translate(WIDTH + BOX, 0), ...bottomRight.translate(WIDTH + BOX, 0), variant === 1],
-            [...topLeft, ...bottomRight, variant === 1],
-            [...bottomRight, ...topRight, variant === 2],
-            [...topLeft.translate(WIDTH + BOX, 0), ...bottomRight.translate(WIDTH + BOX, 0), variant === 1],
-            [...bottomRight.translate(WIDTH + BOX, 0), ...topRight.translate(WIDTH + BOX, 0), variant === 2],
-        ]
+        // Draw top cap
+        if (direction == 1) {
+            shape.beginShape(0, 0)
+            shape.offset(width + BOX_HEIGHT, 0)
+            shape.offset(BOX_HEIGHT, BOX_HEIGHT)
+            shape.offset(0, height - BOX_HEIGHT, true)
+            shape.offset(-width -BOX_HEIGHT, 0)
+            shape.offset(-BOX_HEIGHT, -BOX_HEIGHT)
+            shape.offset(0, -(height - BOX_HEIGHT), true)
+        } else {
+            shape.beginShape(BOX_HEIGHT, 0)
+            shape.offset(0, height - BOX_HEIGHT, true)
+            shape.offset(width + BOX_HEIGHT, 0)
+            shape.offset(-BOX_HEIGHT, BOX_HEIGHT)
+            shape.offset(-width -BOX_HEIGHT, 0)
+            shape.offset(BOX_HEIGHT, -BOX_HEIGHT)
+        }
 
-        var points = [[BOX, BOX]]
-        points.addPoint(BOX + WIDTH, 0)
-        points.addPoint(-BOX, -BOX)
-        points.addPoint(-WIDTH - BOX, 0)
-        points.addPoint(BOX, BOX)
-
-        var shape = createShape(points)
-        lines = shape
+        var lines = shape.get()
 
         for (var i = 0; i < lines.length; i++) {
-            console.log(lines[i])
             var d = lines[i];
             var line = document.createElementNS(ns, "line");
             line.setAttribute("x1", d[0]);
@@ -127,25 +110,38 @@
         return svg;
     }
 
-    function createShape(points) {
-        var lines = []
-        var p1 = points[0]
-
-        for (var i = 1; i < points.length; i++) {
-            var p2 = points[i]
-            var line = [...p1, ...p2]
-            lines.push(line)
-            p1 = p2
+    class Shape {
+        constructor() {
+            this.points = []
+            this.lines = []
         }
 
-        return lines
-    }
+        beginShape(x, y) {
+            this.points.push([x, y])
+        }
 
-    Object.prototype.addPoint = function (x, y) {
-        var point = [...this[this.length - 1]]
-        point[0] += x
-        point[1] += y
-        this.push(point)
+        addLine(idx1, idx2) {
+            let p1 = this.points[idx1]
+            let p2 = this.points[idx2]
+            let line = [p1[0], p1[1], p2[0], p2[1]]
+            this.lines.push(line)
+        }
+
+        offset(x, y, draw = true) {
+            var idx = this.points.length - 1
+            var p = [this.points[idx][0] + x, this.points[idx][1] + y]
+            this.points.push(p)
+            if(draw){
+                this.addLine(this.points.length - 1, this.points.length - 2)
+            }
+        }
+
+        get() {
+            if (this.lines.length == 0) {
+                return null
+            }
+            return this.lines
+        }
     }
 
     function measureTextWidth(text) {
